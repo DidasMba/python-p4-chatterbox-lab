a59139a98f4e61501eb991353602234e6aaa4c6a
DidasMba <daniel.mbarushi@gmail.com>
2024-01-13 10:09:11 +0300

    message

--- server/models.py
+++ server/models.py
@@ -1,14 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
-
-class Message(db.Model, SerializerMixin):
-    __tablename__ = 'messages'
-
+class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.String, default=datetime.utcnow().isoformat())
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow().isoformat(), onupdate=datetime.utcnow())
+
+    def __repr__(self):
+        return f"<Message {self.id}>"
+
